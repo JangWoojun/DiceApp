59a50c5393eecc8841ea901e41cbbc1a820ab642
JangWoojun <woojun0107@naver.com>
2022-12-15 08:55:37 +0900

    FEAT : 완성

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
+    
+    let randomDiceImage : [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        dice1.image = randomDiceImage.randomElement()
+        dice2.image = randomDiceImage.randomElement()
+    }
+    
 }
 
